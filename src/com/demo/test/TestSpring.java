eaad48928688fb8d2693f84f2bbbc9da4a771906
laoji525 <jimji1024@gmail.com>
2019-07-19 16:39:00 -0400

    4.38

--- src/com/demo/test/TestSpring.java
+++ src/com/demo/test/TestSpring.java
@@ -1,5 +1,6 @@
 package com.demo.test;
 
+import com.demo.pojo.Product;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,8 +11,9 @@ public class TestSpring {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
 
-		Category c = (Category) context.getBean("c");
+		Product p = (Product) context.getBean("p");
 
-		System.out.println(c.getName());
+		System.out.println(p.getName());
+		System.out.println(p.getCategory().getName());
 	}
 }
