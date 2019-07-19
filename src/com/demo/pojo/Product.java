33d44b67b2829cd3f4af3c12e2475caf741772f1
laoji525 <jimji1024@gmail.com>
2019-07-19 16:35:07 -0400

    4.35

--- src/com/demo/pojo/Product.java
+++ src/com/demo/pojo/Product.java
@@ -1,4 +1,33 @@
 package com.demo.pojo;
 
 public class Product {
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+    private String name;
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    private Category category;
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
